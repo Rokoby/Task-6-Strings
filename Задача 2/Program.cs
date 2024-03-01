6a21cf8f2ba8f3fe3ad7ee515b523bf33f351cae
BR <244097@niuitmo.ru>
2024-03-01 14:12:12 +0300

    Last version

---
+++ Задача 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string s = Console.ReadLine();
+            string[] stringArray = s.Split(' ');
+            string p = "";
+
+            foreach (string str in stringArray)
+            {
+                p += str;
+            }
+            p = p.ToLower();
+
+            bool k = true;
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i] != p[p.Length - 1 - i])
+                    k = false;                
+            }
+
+            if (k == true)
+                Console.WriteLine("Палиндром");
+            else
+                Console.WriteLine("Не палиндром");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string s = Console.ReadLine();
+            char startDelete = Convert.ToChar("{");
+            char endDelete = Convert.ToChar("}");
+            int counter = 0;
+            string newString = "";
+
+           foreach (char c in s)
+            {
+                if (c == startDelete)
+                    counter++;
+                if (counter == 0)
+                    newString += c;
+                if (c == endDelete)
+                    counter--;
+            }
+            foreach (char c in newString)
+                Console.Write(c);
+            Console.ReadKey();
+        }
+    }
+}
