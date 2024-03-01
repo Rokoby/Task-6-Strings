6ad8d9c9006fdfbe19e4fe461d8e8a0c47ba7e74
BR <244097@niuitmo.ru>
2024-03-01 11:55:50 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string s = Console.ReadLine();
+            string[] stringArray = s.Split(' ');
+            string max = stringArray[0];
+            foreach(string str in stringArray)
+            {
+                if (str.Length > max.Length)
+                {
+                    max = str;
+                }
+            }
+            Console.WriteLine("{0}", max);
+            Console.ReadKey();
+        }
+    }
+}
